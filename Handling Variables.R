cafc0588ad20178a1a5ec213ac7ce6efd1938878
KRISHNA KESARI <33395757+krishnakesari@users.noreply.github.com>
2017-12-14 12:03:04 -0500

    Add files via upload

---
+++ Handling Variables.R
@@ -0,0 +1,94 @@
+## Handling Variables
+
+# Creating factor variable
+set.seed(124)
+schtyp <- sample(0:1, 20, replace = TRUE)
+schtyp
+
+is.factor(schtyp)
+
+is.numeric(schtyp)
+
+# Creating a factor
+schtyp.f <- factor(schtyp, labels = c("private", "public"))
+schtyp.f
+
+is.factor(schtyp.f)
+
+
+# Generating 'string' variable
+
+ses <- c("low", "middle", "low", "low", "low", "low", "middle", "low", "middle",
+         "middle", "middle", "middle", "middle", "high", "high", "low", "middle",
+         "middle", "low", "high")
+is.factor(ses)
+is.character(ses)
+
+# To fix order use 'levels'
+
+ses.f <- factor(ses, levels = c("low", "middle", "high"))
+is.factor(ses.f)
+levels(ses.f)
+
+
+## Creating ordered factor variables
+
+ses.order <- ordered(ses, levels = c("low", "middle", "high"))
+ses
+ses.order
+
+is.factor(ses.order)
+
+## Adding and dropping levels in factor variables
+
+ses.f[21] <- "very.high"
+ses.f
+
+ses.f <- factor(ses.f, levels = c(levels(ses.f), "very.high"))
+ses.f[21] <- "very.high"
+ses.f
+
+levels(ses.f)
+
+ses.f.new <- ses.f[ses.f != "very.high"]
+ses.f.new
+
+ses.f.new <- factor(ses.f.new)
+ses.f.new
+
+levels(ses.f.new)
+
+
+## Example:
+ses.f <- ses.f.new
+read <- c(34, 39, 63, 44, 47, 47, 57, 39, 48, 47, 34, 37, 47, 47, 39, 47,
+          47, 50, 28, 60)
+combo <- data.frame(schtyp, schtyp.f, ses, ses.f, read)
+table(ses, schtyp)
+
+table(ses.f, schtyp.f)
+
+library(lattice)
+bwplot(schtyp ~ read | ses, data = combo, layout = c(2, 2))
+
+bwplot(schtyp.f ~ read | ses.f, data = combo, layout = c(2, 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
